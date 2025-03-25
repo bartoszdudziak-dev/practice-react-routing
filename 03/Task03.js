52714c67ef714de82bea0fdb44b939e6ff1a5272
Bartosz Dudziak <bartoszdudziak.dev@gmail.com>
2025-03-25 23:31:32 +0100

    Add filter function

--- 03/Task03.js
+++ 03/Task03.js
@@ -1,25 +1,25 @@
 import React from "react";
 import products from "./../src/products.json";
 import { useParams } from "react-router-dom";
-import Shop from '../src/components/Shop'
+import Shop from "../src/components/Shop";
 
 const Task03 = () => {
   const { category } = useParams();
 
-  let fitleredProducts;
-
-  if (!category) {
-    fitleredProducts = products;
-  } else {
-    fitleredProducts = products.filter(
-      (product) => product.category.toLowerCase() === category
+  const filterProducts = (products, { filterBy, filterValue }) => {
+    return products.filter(
+      (product) => product[filterBy].toLowerCase() === filterValue
     );
-  }
+  };
+
+  const fitleredProducts = category
+    ? filterProducts(products, { filterBy: "category", filterValue: category })
+    : products;
 
   return (
     <>
       <h1>Task 03</h1>
-      <Shop products={fitleredProducts}/>
+      <Shop products={fitleredProducts} />
     </>
   );
 };
