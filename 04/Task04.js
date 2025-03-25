dfa696aa508d2afe96662eae33f59e1be84eea66
Bartosz Dudziak <bartoszdudziak.dev@gmail.com>
2025-03-25 23:45:11 +0100

    Add sort function

--- 04/Task04.js
+++ 04/Task04.js
@@ -8,25 +8,33 @@ const Task04 = () => {
   const { sortBy } = useParams();
   const history = useHistory();
 
+  const [key = "price", direction = "asc"] = sortBy?.split("-") || [];
+
   const handleChange = (e) => {
     history.push(e.target.value);
   };
 
-  let filteredProduct;
+  const sortProducts = (products, { sortBy, direction }) => {
+    return [...products].sort((a, b) => {
+      if (direction === "asc") {
+        return a[sortBy] - b[sortBy];
+      }
 
-  if (sortBy === "price-desc") {
-    filteredProduct = [...products].sort((a, b) => b.price - a.price);
-  } else if (sortBy === "price-asc") {
-    filteredProduct = [...products].sort((a, b) => a.price - b.price);
-  } else {
-    filteredProduct = products;
-  }
+      if (direction === "desc") {
+        return b[sortBy] - a[sortBy];
+      }
+
+      return products;
+    });
+  };
+
+  const sortedProducts = sortProducts(products, { sortBy: key, direction });
 
   return (
     <>
       <h1>Task04</h1>
 
-      <select onChange={handleChange} defaultValue='/task04'>
+      <select onChange={handleChange} defaultValue="/task04">
         <option value="/task04" disabled hidden>
           Sortuj wedlug
         </option>
@@ -34,7 +42,7 @@ const Task04 = () => {
         <option value="/task04/price-asc">cena rosnąco</option>
       </select>
 
-      <Shop products={filteredProduct} />
+      <Shop products={sortedProducts} />
     </>
   );
 };
